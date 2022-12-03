c93ef1a1f06249c4c5ca29df9c6d490b0866332a
Noapoleon <noalegrand088@gmail.com>
2022-12-03 22:17:23 +0100

    idk what i changed

--- srcs/hook_utils.c
+++ srcs/hook_utils.c
@@ -6,13 +6,58 @@
 /*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 19:26:54 by nlegrand          #+#    #+#             */
-/*   Updated: 2022/12/02 22:27:43 by nlegrand         ###   ########.fr       */
+/*   Updated: 2022/12/03 22:17:05 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <time.h>
 
+#define CLOCKS_PER_MILLI (CLOCKS_PER_SEC / 1000)
+
+void	clear_image(t_fdf *fdf, unsigned int col)
+{
+	const int	tot = fdf->wwidth * fdf->wheight;
+	int			i;
+	char		*dst;
+
+	dst = fdf->buf.addr;
+	i = 0;
+	while (i < tot)
+	{
+//		printf("x: %d, y: %d\n", 
+		dst += fdf->buf.bpp / 8;
+		*(unsigned int *)dst = col;
+		++i;
+	}
+}
+
+int	loop_hook(void *param)
+{
+	t_fdf			*fdf = param;
+	static clock_t	last;
+
+
+	if (last == 0)
+	{
+		last = clock();
+		return (0);
+	}
+	//big if
+	if ((clock() - last) / CLOCKS_PER_MILLI > 1)
+	{
+		last = clock();
+		fdf->mtr[0] = cos(last);
+		fdf->mtr[1] = sin(last);
+		fdf->mtr[2] = -sin(last);
+		fdf->mtr[3] = cos(last);
+		// redraw image
+		clear_image(fdf, 0);
+		map_lines_test(fdf);
+		mlx_put_image_to_window(fdf->id, fdf->win, fdf->buf.img, 0, 0);
+	}
+	return (0);
+}
 int	set_hooks(t_fdf *fdf)
 {
 	mlx_key_hook(fdf->win, &key_hook, fdf);
@@ -32,16 +77,3 @@ int	key_hook(int keycode, void *param)
 	return (0);
 }
 
-int	loop_hook(void *param)
-{
-	t_fdf *fdf = param;
-	static last = clock();
-
-	//big if
-	if ((clock() - last) / (CLOCKS_PER_SEC * 1000) > 100)
-	{
-		last = clock();
-		// redraw image
-		mlx_put_image_to_window(fdf->id, fdf->win, fdf->buf.img, 0, 0);
-	}
-}
--- srcs/test_utils.c
+++ srcs/test_utils.c
@@ -6,7 +6,7 @@
 /*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 18:40:01 by nlegrand          #+#    #+#             */
-/*   Updated: 2022/12/02 22:02:28 by nlegrand         ###   ########.fr       */
+/*   Updated: 2022/12/03 22:17:05 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,11 +87,11 @@ void	calc_coords(t_fdf *fdf)
 			fdf->vs[y][x].x -= WIN_WIDTH / 2;
 			fdf->vs[y][x].y -= WIN_HEIGHT / 2;
 			tmp = fdf->vs[y][x].x;
-			fdf->vs[y][x].x = fdf->mtr[0] * tmp + fdf->mtr[1] * fdf->vs[y][x].y;
-			fdf->vs[y][x].y = fdf->mtr[2] * tmp + fdf->mtr[3] * fdf->vs[y][x].y;
-			fdf->vs[y][x].y *= 0.5;
-			fdf->vs[y][x].x += WIN_WIDTH / 2;
-			fdf->vs[y][x].y += WIN_HEIGHT / 2 - fdf->vs[y][x].z * fdf->zsize;
+			//fdf->vs[y][x].x = fdf->mtr[0] * tmp + fdf->mtr[1] * fdf->vs[y][x].y;
+			//fdf->vs[y][x].y = fdf->mtr[2] * tmp + fdf->mtr[3] * fdf->vs[y][x].y;
+			//fdf->vs[y][x].y *= 0.5;
+			//fdf->vs[y][x].x += WIN_WIDTH / 2;
+			//fdf->vs[y][x].y += WIN_HEIGHT / 2 - fdf->vs[y][x].z * fdf->zsize;
 			++x;
 		}
 		++y;
